2effe871ee34381206fc40264d0e24ebb5b1e74a
ultrasoft011 <ultrasoft.011@gmail.com>
2021-05-02 22:35:27 -0500

    01-color-flipper simple colors

--- 01-color-flipper/app.js
+++ 01-color-flipper/app.js
@@ -1,1 +1,10 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function () {
+    // get a random number 0 - 3 to get color from the array "colors"
+    let randomNumber = Math.floor(Math.random() * 4);
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+})
