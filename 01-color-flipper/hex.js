079559e38b748f1767b940a09f4a23a6fef4c40e
ultrasoft011 <ultrasoft.011@gmail.com>
2021-05-19 23:05:34 -0500

    starting hexColor

--- 01-color-flipper/hex.js
+++ 01-color-flipper/hex.js
@@ -1,1 +1,12 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+console.log(btn);
+console.log(color);
+
+btn.addEventListener('click', function() {
+    let hexColor = "#";
+    for (let i = 0; i < 6; i++) {
+        hexColor += hex[11];
+    }
+})
