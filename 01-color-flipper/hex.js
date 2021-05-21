0d596411e8d754543630960edb985fbe54178782
ultrasoft011 <ultrasoft.011@gmail.com>
2021-05-20 23:15:02 -0500

    hexColor

--- 01-color-flipper/hex.js
+++ 01-color-flipper/hex.js
@@ -3,10 +3,14 @@ const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 console.log(btn);
 console.log(color);
-
+// The hex color will be like #f45011
 btn.addEventListener('click', function() {
     let hexColor = "#";
     for (let i = 0; i < 6; i++) {
-        hexColor += hex[11];
+        hexColor += hex[0];
     }
+    // To modify the color reference (text)
+    color.textContent = hexColor;
+    // Change the background of the body by the hexColor
+    document.body.style.backgroundColor = hexColor;
 })
