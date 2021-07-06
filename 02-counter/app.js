20acd8eb6b059deec91734a9e0482b9ac72e0a6e
ultrasoft011 <ultrasoft.011@gmail.com>
2021-07-06 15:35:52 -0500

    reestructuring the functions

--- 02-counter/app.js
+++ 02-counter/app.js
@@ -1,17 +1,54 @@
-const counter = document.querySelector('.counter');
-const increase = document.querySelector('.increase');
-const decrease = document.querySelector('.decrease');
+// const counter = document.querySelector('.counter');
+// const increase = document.querySelector('.increase');
+// const decrease = document.querySelector('.decrease');
+// const reset = document.querySelector('.reset')
 
-console.log(decrease);
+// console.log(decrease);
 
-let number = 0;
+// let number = 0;
 
-increase.addEventListener('click', function() {
-    number += 1;
-    counter.textContent = number;
-})
+// // Increase function
 
-decrease.addEventListener('click', () => {
-    number -= 1;
-    counter.textContent = number;
-})
+// increase.addEventListener('click', function() {
+//     number += 1;
+//     counter.textContent = number;
+// })
+
+// // Decrease function
+
+// decrease.addEventListener('click', () => {
+//     number -= 1;
+//     counter.textContent = number;
+// })
+
+// // Reset function
+
+// reset.addEventListener('click', () => {
+//     number = 0;
+//     counter.textContent = number;
+// })
+
+// Option B: using forEach
+
+let counter = 0;
+const counterNumber = document.querySelector(".counterNumber");
+
+const buttons = document.querySelectorAll(".btn");
+console.log(buttons);
+
+buttons.forEach((e) =>
+  e.addEventListener("click", function (e) {
+    // Current Target will get object info when the user clicks something in the web page
+    const value = e.currentTarget.classList;
+    console.log(value);
+    // Cointains method will check if an specific word exists in one of the positions of the array
+    if (value.contains("increase")) {
+      counter += 1;
+    } else if (value[1] === "decrease") {
+      counter -= 1;
+    } else if (value.contains("reset")) {
+      counter = 0;
+    }
+    counterNumber.textContent = counter;
+  })
+);
